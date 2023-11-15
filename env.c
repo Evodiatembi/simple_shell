3d9c84e4d7ee86fc457df90cbe7938df9525276b
Evodiatembi <evodytembi@gmail.com>
2023-11-15 13:53:38 -0800

    env.c

---
+++ env.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+*main - environment function
+*@ac: parameter one
+*@av: parameter two
+*Return: always 0
+*/
+int main(int ac, char *av)
+char *environmen_variable
+while (environment_variable[m] != null)
+{
+write("%d: %s\n", m, environment_variable[m]);
+m++;
+}
+return (0);
+}
